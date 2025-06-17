85fbace7f233038943a3c01c22fdd5398eca89e7
root <imrannazir2102@gmail.com>
2025-06-17 07:17:52 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+app = Flask(_name_)
+
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "Hello from Flask app running in Docker!"
+    })
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
